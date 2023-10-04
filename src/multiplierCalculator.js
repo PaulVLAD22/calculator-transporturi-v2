a46faa3f6d076329ee3446666f84f2ac7e602cef
vlad <vlad.munteanu@orange.com>
2023-10-04 17:34:26 +0300

    feat: update prices

--- src/multiplierCalculator.js
+++ src/multiplierCalculator.js
@@ -1,4 +1,6 @@
 export const getMultiplier = (
+  tripType,
+  externalTripType,
   carFrigoAdrNormalType,
   carType,
   carTransportationType
@@ -6,34 +8,49 @@ export const getMultiplier = (
   console.log(carFrigoAdrNormalType);
   console.log(carType);
   console.log(carTransportationType);
+  let multipier = 1;
+
+  if (tripType === "extern") {
+    if (carType === "sprinter" && carTransportationType === "grupaj") {
+      multipier *= 1.2;
+    }
+    if (
+      carType === "camion" &&
+      externalTripType === "import" &&
+      carTransportationType === "grupaj"
+    ) {
+      multipier *= 1.3;
+    }
+  }
+
   if (carFrigoAdrNormalType === "frigo") {
     if (carType === "camion") {
       if (carTransportationType === "complet") {
-        return 1.3;
+        multipier *= 1.3;
       } else if (carTransportationType === "grupaj") {
-        return 1.5;
+        multipier *= 1.5;
       }
     } else if (carType === "sprinter") {
       if (carTransportationType === "complet") {
-        return 1.5;
+        multipier *= 1.5;
       } else if (carTransportationType === "grupaj") {
-        return 2;
+        multipier *= 2;
       }
     }
   } else if (carFrigoAdrNormalType === "adr") {
     if (carType === "camion") {
       if (carTransportationType === "complet") {
-        return 1.15;
+        multipier *= 1.15;
       } else if (carTransportationType === "grupaj") {
-        return 1.4;
+        multipier *= 1.4;
       }
     } else if (carType === "sprinter") {
       if (carTransportationType === "complet") {
-        return 1.3;
+        multipier *= 1.3;
       } else if (carTransportationType === "grupaj") {
-        return 1.7;
+        multipier *= 1.7;
       }
     }
   }
-  return 1;
+  return multipier;
 };
